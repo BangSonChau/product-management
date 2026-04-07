eb7f202945095e4d589cf1186e4a244470a8f398
Châu Băng Sơn <bangson.chau@gmail.com>
2026-04-08 00:55:35 +0700

    Tối ưu phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -25,30 +26,21 @@ module.exports.index = async (req, res) => {
   }
 
   //Pagination
-  let objectPagination = {
-    currentPage: 1,
-    limitItem: 4,
-  };
+  const countProducts = await Product.countDocuments(find);
 
-  if (req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-
-  console.log(objectPagination.currentPage);
-
-  objectPagination.skipItem =
-    (objectPagination.currentPage - 1) * objectPagination.limitItem;
+  let objectPagination = paginationHelper(
+    req.query, 
+    {
+      currentPage: 1,
+      limitItem: 4,
+    },
+    countProducts    
+  );
   //EndPagination
 
   const products = await Product.find(find)
     .limit(objectPagination.limitItem)
     .skip(objectPagination.skipItem);
-
-  const countProducts = await Product.countDocuments(find);
-
-  const totalPage = Math.ceil(countProducts / objectPagination.limitItem);
-
-  objectPagination.totalPage = totalPage;
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
---
+++ helpers/pagination.js
@@ -0,0 +1,17 @@
+module.exports =(query, objectPagination, countProducts) => {
+
+  if (query.page) {
+    objectPagination.currentPage = parseInt(query.page);
+  }
+
+  console.log(objectPagination.currentPage);
+
+  objectPagination.skipItem =
+    (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+  const totalPage = Math.ceil(countProducts / objectPagination.limitItem);
+
+  objectPagination.totalPage = totalPage;
+
+  return objectPagination;
+};
