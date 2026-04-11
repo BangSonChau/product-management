68bd0b5a4194e8347abb05e39d11876d164fc1ed
Châu Băng Sơn <bangson.chau@gmail.com>
2026-04-11 23:07:10 +0700

    Tính năng tạo mới sản phẩm

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
+const storageMulter = require("../../helpers/storage-multer");
+const upload = multer({ storage: storageMulter() });
 
-const controller = require("../../controllers/admin/product.controller"); ;
+const controller = require("../../controllers/admin/product.controller");
 
 router.get("/", controller.index);
 
@@ -13,5 +16,5 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
 
-router.post("/create", controller.createPost);
+router.post("/create", upload.single("thumbnail"), controller.createPost);
 module.exports = router;
