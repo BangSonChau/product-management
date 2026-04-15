56843000f3faf36813277ede81d03e1134722f12
Châu Băng Sơn <bangson.chau@gmail.com>
2026-04-15 15:44:48 +0700

    trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -2,20 +2,44 @@ const Product = require("../../models/product.model");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
-
   const products = await Product.find({
     // Bộ lọc các sản phẩm có trường
     status: "active",
-    deleted: false
+    deleted: false,
   }).sort({ position: "desc" });
 
-  const newProducts = products.map(item => {
-    item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(2);
+  const newProducts = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(2);
     return item;
-  })
-  
+  });
+
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
-    products: newProducts
+    products: newProducts,
   });
-}
+};
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
+
+    const product = await Product.findOne(find);
+
+    console.log(product);
+
+    res.render("client/pages/products/detail", {
+      pageTitle: "Chi tiết sản phẩm",
+      product: product,
+    });
+  } catch (error) {
+    res.redirect("/products");
+  }
+};
